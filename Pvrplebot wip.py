2b440f4d4e5b4847a7229893d5a0a8e7768317f5
Henry <Pvrpl3Afr0@users.noreply.github.com>
2017-02-01 08:31:03 -0500

    Add files via upload

---
+++ Pvrplebot wip.py
@@ -0,0 +1,21 @@
+#First version of Pvrple.
+#pvrplebot
+from random import *
+#Defining stuff.
+'''
+def PassgenPage(Password):
+    '''
+#Main page.
+prompt = input("Hi, I'm pvrplebot! Ask me A, B or C! ")
+
+if prompt == "a" or prompt == "A":
+    print("My name is Pvrplebot! Nice to meet you!")
+elif prompt == "b" or prompt == "B":
+    print("I was written by Pvrpl3afr0!")
+elif prompt == "c" or prompt == "C":
+    print("I don't do much at the moment...")
+else:
+    print("I said, A B or C! try again..")
+    '''
+while prompt == "a" or prompt == "A":
+'''
